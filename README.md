b4195da753118d2d59b224f4967bca6c8ca4fcda
marble <1218892028@qq>
2020-04-11 12:34:10 +0800

    1.初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # SpringBootLearnMook
 基于模块demo
+
+1. 第一个spring boot项目
+    * 定义用户模型
+    * 客户端发送POST,创建用户(Web MVC)
+    * 客户端发送GET,获取所有用户(Web Flux)
---
+++ src/test/java/com/marble/FirstAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
